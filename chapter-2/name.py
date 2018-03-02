abbf0f6d0cc71c22ba12711a06af94d9343e7f6d
Devin Miller <devinmiller@gmail.com>
2018-03-02 15:08:04 -0500

    Started chapter 2 Modified hello_world/py and addedname.py Added birthday.py to purposfully show a Type Error

---
+++ chapter-2/name.py
@@ -0,0 +1,11 @@
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "devin"
+last_name = "miller"
+full_name = first_name + " " + last_name
+
+print(full_name)
+print(first_name + " " + last_name)
