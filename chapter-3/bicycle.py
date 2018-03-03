f4c9a56840bb7f0afcd4de3f5b735092b448faef
Devin Miller <devinmiller@gmail.com>
2018-03-02 20:13:18 -0500

    added code to print various elements in a list

--- chapter-3/bicycle.py
+++ chapter-3/bicycle.py
@@ -1,2 +1,9 @@
 bicycles = ['trek', 'cannondale', 'redline', 'speacialized']
-print(bicycles)
+print(bicycles[0].title()) # Access the first element of the list
+print(bicycles[1])
+print(bicycles[3])
+
+print(bicycles[-1]) # This will print the last element of list
+
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
