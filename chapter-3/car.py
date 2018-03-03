0d20a16d731e1ec6207c861c707ae76fe4912987
Devin Miller <devinmiller@gmail.com>
2018-03-02 21:03:40 -0500

    added code to learn how to manipulate lists in different ways

--- chapter-3/car.py
+++ chapter-3/car.py
@@ -1,3 +1,14 @@
 cars = ['bmw', 'audi', 'toyota', 'subaru']
-cars.sort() # This will sort the list in-place
+
+print(sorted(cars)) # This will sort the list but not modify it
+
+cars.reverse()  # This will revers the order of the list in-place
 print(cars)
+
+cars.sort() # This will sort the list in-place (modify it)
+print(cars)
+
+cars.sort(reverse=True) #This will sort the list in reverse
+print(cars)
+
+print(len(cars))    # This will print the number (length) of the list
