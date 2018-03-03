40b6fc45fbca350d33e1e78f262dbec994d788ad
Devin Miller <devinmiller@gmail.com>
2018-03-02 20:28:36 -0500

    added code for del or popping elements in list of motorcycles.py

--- chapter-3/motorcycles.py
+++ chapter-3/motorcycles.py
@@ -6,3 +6,16 @@ print(motorcyles)
 
 motorcyles.append('triumph')    # This will add an element at end
 print(motorcyles)
+
+del motorcyles[0]   # This will del the entry at index 0
+print(motorcyles)
+
+popped_motorcyle = motorcyles.pop() # This will remove last item in motorcyles
+print(motorcyles)
+print(popped_motorcyle)
+
+popped = motorcyles.pop(1)  # This will pop the 2nd element in list
+print(popped)
+
+motorcyles.remove('HD') # This will remove the item by value
+print(motorcyles)
