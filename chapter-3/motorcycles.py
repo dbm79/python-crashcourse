ebe666aceda33e7ebcec1e37048da29d081eb61a
Devin Miller <devinmiller@gmail.com>
2018-03-02 20:20:27 -0500

    Initial commit for motorcycles.py

---
+++ chapter-3/motorcycles.py
@@ -0,0 +1,8 @@
+motorcyles = ['honda', 'yamaha', 'suzuki', 'HD']
+print(motorcyles)
+
+motorcyles[0] = 'ducati'    #This will replace the first element
+print(motorcyles)
+
+motorcyles.append('triumph')    # This will add an element at end
+print(motorcyles)
