f46a77b094224c23fafbcab1ae410ec5a3201a62
Devin Miller <devinmiller@gmail.com>
2018-03-03 20:21:44 -0500

    initial commit 4-11-pizzas.py

---
+++ chapter-4/4-11-pizzas.py
@@ -0,0 +1,13 @@
+my_pizzas = ['pepperoni', 'classic trio', 'hawaian']
+friends_pizzas = my_pizzas[:]
+
+my_pizzas.append('mushroom')
+friends_pizzas.append('cheese')
+
+print(my_pizzas)
+print(friends_pizzas)
+
+"""
+    for pizza in pizzas:
+        print("I like to eat {} pizza!".format(pizza.title()) )
+"""
