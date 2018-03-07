da91bb10bf40523ceef87d70123b434c37efa0df
Devin Miller <devinmiller@gmail.com>
2018-03-06 21:37:26 -0500

    Initil commit for dimensions.py and 4-13-buffet.py

---
+++ chapter-4/4-13-buffet.py
@@ -0,0 +1,9 @@
+buffet_foods = ('meat', 'rolls', 'salad', 'mashed potatoes', 'fruit')
+
+for food in buffet_foods:
+    print(food)
+
+buffet_foods = ('tofu', 'veggies', 'salad', 'mashed potatoes', 'fruit')
+
+for food in buffet_foods:
+    print(food)
