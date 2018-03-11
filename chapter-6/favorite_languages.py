51305093d4c893d05cd9fcd7bbfd489762cf6789
Devin Miller <devinmiller@gmail.com>
2018-03-11 12:37:39 -0400

    odified dictionart to have list valued and looped through them

--- chapter-6/favorite_languages.py
+++ chapter-6/favorite_languages.py
@@ -1,15 +1,17 @@
 favorite_languages = {
-    'jen': 'python',
-    'sarah': 'c',
-    'edward': 'ruby',
-    'phil': 'python'
+    'jen': ['python', 'ruby'],
+    'sarah': ['c'],
+    'edward': ['ruby', 'go'],
+    'phil': ['python', 'haskel']
     }
 
-print("Sarah's favorite language is: " + favorite_languages['sarah'].title() + ".")
-
-for name in favorite_languages.keys():
-    print(name.title())
+#print("Sarah's favorite language is: " + favorite_languages['sarah'].title() + ".")
 
 #This will sort the keys then print
-for name in sorted(favorite_languages.keys()):
-    print(name.title())
+#for name in sorted(favorite_languages.keys()):
+#    print(name.title())
+
+for name, languages in favorite_languages.items():
+    print("\n" + name.title() + "'s favorite languages are:")
+    for language in languages:
+        print('\t' + language.title())
