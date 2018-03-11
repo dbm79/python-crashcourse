ca764c5fd70f8c7fb2b343f160173940d0d0c3b6
Devin Miller <devinmiller@gmail.com>
2018-03-11 12:01:46 -0400

    inital commit for user.py

---
+++ chapter-6/user.py
@@ -0,0 +1,11 @@
+user_0 = {
+    'username': 'devinmiller',
+    'first_name': 'devin',
+    'last_name': 'miller'
+    }
+
+print(user_0)
+
+for key, value in user_0.items():
+    print('\nKey: ' + key)
+    print('Value: ' + value)
