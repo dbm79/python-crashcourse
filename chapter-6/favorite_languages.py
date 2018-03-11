97eac203b16f346ee75ed2a5046ea84af0f34f4e
Devin Miller <devinmiller@gmail.com>
2018-03-11 12:07:16 -0400

    added loop to sort keys

--- chapter-6/favorite_languages.py
+++ chapter-6/favorite_languages.py
@@ -9,3 +9,7 @@ print("Sarah's favorite language is: " + favorite_languages['sarah'].title() + "
 
 for name in favorite_languages.keys():
     print(name.title())
+
+#This will sort the keys then print
+for name in sorted(favorite_languages.keys()):
+    print(name.title())
