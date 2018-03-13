578e51c10f41aba3447f8bf2ef2e3402e4580fe5
Devin Miller <devinmiller@gmail.com>
2018-03-12 20:18:23 -0400

    added keyword argument example

--- chapter-8/pets.py
+++ chapter-8/pets.py
@@ -4,3 +4,6 @@ def describe_pets(animal_type, pet_name):
     print('My ' + animal_type + "'s name is " + pet_name.title() )
 
 describe_pets('dog', 'hulk')
+
+#Example of passing keyword arguments
+describe_pets(animal_type='dog', pet_name='daisy')
