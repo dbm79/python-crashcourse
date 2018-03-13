bd6507f915f1773ec7406ba961b912c17ec0ddf9
Devin Miller <devinmiller@gmail.com>
2018-03-12 20:14:39 -0400

    initial commit pets.py

---
+++ chapter-8/pets.py
@@ -0,0 +1,6 @@
+def describe_pets(animal_type, pet_name):
+    '''Display information about a pet.'''
+    print('\nI have a ' + animal_type + '.')
+    print('My ' + animal_type + "'s name is " + pet_name.title() )
+
+describe_pets('dog', 'hulk')
