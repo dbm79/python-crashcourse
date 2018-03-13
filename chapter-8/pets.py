db5ae49ebb66debb895b54f3e05b5a9cf044639b
Devin Miller <devinmiller@gmail.com>
2018-03-12 20:28:20 -0400

    added example of default value

--- chapter-8/pets.py
+++ chapter-8/pets.py
@@ -1,4 +1,4 @@
-def describe_pets(animal_type, pet_name):
+def describe_pets(pet_name, animal_type='dog'):
     '''Display information about a pet.'''
     print('\nI have a ' + animal_type + '.')
     print('My ' + animal_type + "'s name is " + pet_name.title() )
@@ -7,3 +7,6 @@ describe_pets('dog', 'hulk')
 
 #Example of passing keyword arguments
 describe_pets(animal_type='dog', pet_name='daisy')
+
+#example of calling function with default value
+describe_pets(pet_name='tank')
