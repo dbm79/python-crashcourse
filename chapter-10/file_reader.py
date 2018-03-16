9699301b7a4db820675b2b833f85093ef81d995c
Devin Miller <devinmiller@gmail.com>
2018-03-15 20:52:52 -0400

    read the lines of a file and print some output

--- chapter-10/file_reader.py
+++ chapter-10/file_reader.py
@@ -1,3 +1,8 @@
 with open('pi_digits.txt') as f_obj:
-    contents = f_obj.read()
-    print(contents)
+    lines = f_obj.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.rstrip()
+print(pi_string)
+print(len(pi_string))
