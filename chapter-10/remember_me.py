c5a39922017ea73b9be6c12939c85693123bd50a
Devin Miller <devinmiller@gmail.com>
2018-03-20 13:41:27 -0400

    initial commit remeber_me.py

---
+++ chapter-10/remember_me.py
@@ -0,0 +1,16 @@
+import json
+
+# load the username if it has been sotred previously
+# otherwise, prompt for the username and store it
+filename  = 'username.json'
+
+try:
+    with open(filename) as f_obj:
+        username = json.load(f_obj)
+except FileNotFoundError:
+        username = input("What is your username? ")
+        with open(filename, 'w') as f_obj:
+            json.dump(username, f_obj)
+            print('We will remember you next tume ' + username + '.')
+else:
+    print('Welcome back ' + username + '.')
