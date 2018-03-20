cf5ed6e563d9bf9a5ba22f377bc90dc24862b5ce
Devin Miller <devinmiller@gmail.com>
2018-03-20 15:10:54 -0400

    refactored program into a function

--- chapter-10/remember_me.py
+++ chapter-10/remember_me.py
@@ -1,16 +1,19 @@
 import json
 
-# load the username if it has been sotred previously
-# otherwise, prompt for the username and store it
-filename  = 'username.json'
+def greet_user():
+    # load the username if it has been sotred previously
+    # otherwise, prompt for the username and store it
+    filename  = 'username.json'
 
-try:
-    with open(filename) as f_obj:
-        username = json.load(f_obj)
-except FileNotFoundError:
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except FileNotFoundError:
         username = input("What is your username? ")
         with open(filename, 'w') as f_obj:
             json.dump(username, f_obj)
             print('We will remember you next tume ' + username + '.')
-else:
-    print('Welcome back ' + username + '.')
+    else:
+        print('Welcome back ' + username + '.')
+
+greet_user()
