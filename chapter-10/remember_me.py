7329b9eb29a541cbf88b0e15931d393f9fee38a1
Devin Miller <devinmiller@gmail.com>
2018-03-20 21:36:14 -0400

    refactored and added get_stored_username function

--- chapter-10/remember_me.py
+++ chapter-10/remember_me.py
@@ -1,19 +1,27 @@
 import json
 
-def greet_user():
-    '''Greet the user by name'''
-    
+def get_stored_username():
+    '''Get stored username if available.'''
     filename  = 'username.json'
 
     try:
         with open(filename) as f_obj:
             username = json.load(f_obj)
     except FileNotFoundError:
-        username = input("What is your username? ")
+        return None
+    else:
+        return username
+
+def greet_user():
+    '''Greet the user by name'''
+    username = get_stored_username()
+    if username:
+        print('Welcome back ' + username + '.')
+    else:
+        username = input('What is your username? ')
+        filename  = 'username.json'
         with open(filename, 'w') as f_obj:
             json.dump(username, f_obj)
             print('We will remember you next tume ' + username + '.')
-    else:
-        print('Welcome back ' + username + '.')
 
 greet_user()
