6cdde0394a64b05f93ca25506ba5cc19154093a9
Devin Miller <devinmiller@gmail.com>
2018-03-22 15:45:30 -0400

    1 milion rolls

--- chapter-15/die_visual.py
+++ chapter-15/die_visual.py
@@ -7,7 +7,7 @@ die_2=Die(10)
 
 #Make some rolls and store them in a list
 results = []
-for roll_num in range(50000):
+for roll_num in range(1000000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -21,10 +21,10 @@ for value in range(2, max_result+1):
 # Visualize the results
 hist = pygal.Bar()
 
-hist._title = 'Results of rolling a D6 and D10 50000 times.'
+hist._title = 'Results of rolling a D6 and D10 1000000 times.'
 hist.x_labels=['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16']
 hist.x_title='Results'
 hist.y_title='Frequency of Result'
 
-hist.add('D6+D6', frequencies)
+hist.add('D6+D10', frequencies)
 hist.render_to_file('die_visual.svg')
