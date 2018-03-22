c9a3f684dae6868910317c6835c73a84c221f7ba
Devin Miller <devinmiller@gmail.com>
2018-03-21 20:35:07 -0400

    changed plot size and resolution

--- chapter-15/rw_visual.py
+++ chapter-15/rw_visual.py
@@ -5,12 +5,15 @@ from random_walk import RandomWalk
 # Make a random walk and plot the points
 while True:
 
-    walk = RandomWalk()
+    walk = RandomWalk(50000)
     walk.fill_walk()
 
     point_numbers = list(range(walk.num_points))
 
-    plt.scatter(walk.x_values, walk.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    # Set the size of the plot window
+    plt.figure(dpi=128, figsize=(10, 6))
+
+    plt.scatter(walk.x_values, walk.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
     plt.scatter(walk.x_values[-1], walk.y_values[-1], c='red', edgecolors='none', s=100)
 
