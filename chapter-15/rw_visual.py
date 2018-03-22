723a01f81410c64ba4a54116e87a06bec38b697c
Devin Miller <devinmiller@gmail.com>
2018-03-21 20:28:59 -0400

    removed x and y axis

--- chapter-15/rw_visual.py
+++ chapter-15/rw_visual.py
@@ -13,6 +13,10 @@ while True:
     plt.scatter(walk.x_values, walk.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
     plt.scatter(walk.x_values[-1], walk.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # Remove the axes
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
     plt.show()
 
     keep_running = input('Make another walk? (y/n) ')
