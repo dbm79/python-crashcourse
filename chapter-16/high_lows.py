55a66e1f000d69434542495f8cde0225eb36ae20
Devin Miller <devinmiller@gmail.com>
2018-03-23 13:56:31 -0400

    fixed typos

--- chapter-16/high_lows.py
+++ chapter-16/high_lows.py
@@ -11,18 +11,18 @@ with open(filename) as f_obj:
     # Get the highs for the days
     dates, highs = [], []
     for row in reader:
-        current_date = datetime.strptime(row[0], '%Y-%m-%d')
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
 
         high = int(row[1])
         highs.append(high)
-print(highs)
 
 # Plot data
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c='red')
 plt.title('Daily high temperatures, July 2014', fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
