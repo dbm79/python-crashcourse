c15d77d1d34ff740f580ee41c52af3f9daa222fe
Devin Miller <devinmiller@gmail.com>
2018-03-23 13:43:28 -0400

    added plot of high temperatures

--- chapter-16/high_lows.py
+++ chapter-16/high_lows.py
@@ -1,4 +1,5 @@
 import csv
+from matplotlib import pyplot as plt
 
 filename = 'sitka_weather_07-2014.csv'
 
@@ -12,3 +13,13 @@ with open(filename) as f_obj:
         high = int(row[1])
         highs.append(high)
 print(highs)
+
+# Plot data
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(highs)
+plt.title('Daily high tempuratures, July 2014', fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel('Tempurature (F)', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
