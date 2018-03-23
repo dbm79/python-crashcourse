3d48be78eeb7e427f1e75da73929beafb4dd995b
Devin Miller <devinmiller@gmail.com>
2018-03-23 13:51:25 -0400

    added dates to plot from csv data

--- chapter-16/high_lows.py
+++ chapter-16/high_lows.py
@@ -1,5 +1,6 @@
 import csv
 from matplotlib import pyplot as plt
+from datetime import datetime
 
 filename = 'sitka_weather_07-2014.csv'
 
@@ -8,18 +9,21 @@ with open(filename) as f_obj:
     header_row = next(reader)
 
     # Get the highs for the days
-    highs = []
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[0], '%Y-%m-%d')
+        dates.append(current_date)
+
         high = int(row[1])
         highs.append(high)
 print(highs)
 
 # Plot data
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs)
-plt.title('Daily high tempuratures, July 2014', fontsize=24)
+plt.plot(dates, highs, c='red')
+plt.title('Daily high temperatures, July 2014', fontsize=24)
 plt.xlabel('', fontsize=16)
-plt.ylabel('Tempurature (F)', fontsize=16)
+plt.ylabel('Temperature (F)', fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
 plt.show()
