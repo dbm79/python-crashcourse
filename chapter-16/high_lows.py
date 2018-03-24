111c14374dc10772aeecad6c712e2b91e54fa6dc
Devin Miller <devinmiller@gmail.com>
2018-03-23 21:00:54 -0400

    added data for whole year

--- chapter-16/high_lows.py
+++ chapter-16/high_lows.py
@@ -2,7 +2,7 @@ import csv
 from matplotlib import pyplot as plt
 from datetime import datetime
 
-filename = 'sitka_weather_07-2014.csv'
+filename = 'sitka_weather_2014.csv'
 
 with open(filename) as f_obj:
     reader = csv.reader(f_obj)
@@ -24,7 +24,7 @@ with open(filename) as f_obj:
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c='red')
 plt.plot(dates, lows, c='blue')
-plt.title('Daily high temperatures, July 2014', fontsize=24)
+plt.title('Daily high temperatures, 2014', fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=16)
