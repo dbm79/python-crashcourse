e6f841d7a0b6f9c5a056b28d82643875979781c3
Devin Miller <devinmiller@gmail.com>
2018-03-24 12:10:58 -0400

    corrected json.dump to json.load

--- chapter-16/world_population.py
+++ chapter-16/world_population.py
@@ -4,7 +4,7 @@ import json
 filename = 'population_data.json'
 
 with open(filename) as f_obj:
-    pop_data = json.dump(f_obj)
+    pop_data = json.load(f_obj)
 
 # Load 2010 population pop_data
 for pop_dict in pop_data:
