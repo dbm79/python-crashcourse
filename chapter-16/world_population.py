db18e43eb14586612fc5d7ed2a6bc8ecf8523210
Devin Miller <devinmiller@gmail.com>
2018-03-24 12:19:54 -0400

    pulled out population data for 2010

--- chapter-16/world_population.py
+++ chapter-16/world_population.py
@@ -8,4 +8,7 @@ with open(filename) as f_obj:
 
 # Load 2010 population pop_data
 for pop_dict in pop_data:
-    print(pop_dict)
+    if pop_dict['Year'] == 2010:
+        country_name = pop_dict['Country Name']
+        population = pop_dict['Value']
+        print(country_name + ': ' + str(population))
