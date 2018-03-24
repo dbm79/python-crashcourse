805a2b51bc2083bb00bcba8b83d7f3e994583d10
Devin Miller <devinmiller@gmail.com>
2018-03-24 12:10:05 -0400

    initial commit world_population.py

---
+++ chapter-16/world_population.py
@@ -0,0 +1,11 @@
+import json
+
+# Load the data into a list
+filename = 'population_data.json'
+
+with open(filename) as f_obj:
+    pop_data = json.dump(f_obj)
+
+# Load 2010 population pop_data
+for pop_dict in pop_data:
+    print(pop_dict)
