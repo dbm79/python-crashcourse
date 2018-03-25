5619ffc49e8c7d46f656daa353e19330c2bb64d0
Devin Miller <devinmiller@gmail.com>
2018-03-25 11:47:55 -0400

    added error checking in country_codes.py

--- chapter-16/country_codes.py
+++ chapter-16/country_codes.py
@@ -2,8 +2,10 @@ from pygal.maps.world import COUNTRIES
 
 def get_country_code(country_name):
     ''' Return the Pygal 2-digit country code for the given country '''
+
     for code, name in COUNTRIES.items():
+        # Go through the dictionary looking for the country name
         if name == country_name:
             return code
-
-    return None
+        else:
+            return "Country Name Not Found!"
