2f8d922db6f1b97902a316528cf24cfbbd5bbb92
Devin Miller <devinmiller@gmail.com>
2018-03-25 20:15:06 -0400

    added world map pygal opbject

--- chapter-16/world_population.py
+++ chapter-16/world_population.py
@@ -18,3 +18,9 @@ for pop_dict in pop_data:
 
         if code:
             cc_populations[code] = population
+
+wm = World()
+wm.title = 'World Population in 2010, by Country '
+wm.add('2010', cc_populations)
+
+wm.render_to_file('world_population.svg')
