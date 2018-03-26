c2ac42f610991fd0cfd80acbb4ca7f3433a31868
Devin Miller <devinmiller@gmail.com>
2018-03-25 20:12:17 -0400

    create a dictionary of country codes and popualtion for graphing

--- chapter-16/world_population.py
+++ chapter-16/world_population.py
@@ -1,5 +1,6 @@
 import json
 import country_codes
+from pygal.maps.world import World
 
 # Load the data into a list
 filename = 'population_data.json'
@@ -7,14 +8,13 @@ filename = 'population_data.json'
 with open(filename) as f_obj:
     pop_data = json.load(f_obj)
 
-# Load 2010 population pop_data
+# build a dictionary of population data
+cc_populations = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == 2010:
         country_name = pop_dict['Country Name']
         population = int(pop_dict['Value'])
-
         code = country_codes.get_country_code(country_name)
+
         if code:
-            print(code + ': ' + str(population))
-        else:
-            print('ERROR - ' + country_name)
+            cc_populations[code] = population
