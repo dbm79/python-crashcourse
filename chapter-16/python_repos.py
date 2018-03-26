c05fa5cc323d7eb4f16d6746f74645468a9ab158
Devin Miller <devinmiller@gmail.com>
2018-03-26 19:35:46 -0400

    stored response into a dictionary

--- chapter-16/python_repos.py
+++ chapter-16/python_repos.py
@@ -3,3 +3,9 @@ import requests
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
 print('Status Code: ', r.status_code)
+
+#Store API response in a dictionary
+response_dict = r.json()
+
+#Process the results
+print(response_dict.keys())
