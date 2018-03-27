d02f678da712939924d86b9a2369d6bbd10e9cd1
Devin Miller <devinmiller@gmail.com>
2018-03-26 20:19:04 -0400

    explored some of the info returned in the API request

--- chapter-16/python_repos.py
+++ chapter-16/python_repos.py
@@ -6,6 +6,14 @@ print('Status Code: ', r.status_code)
 
 #Store API response in a dictionary
 response_dict = r.json()
+print('Total Repositories: ', response_dict['total_count'])
 
-#Process the results
-print(response_dict.keys())
+#Explore information about the repositories
+repo_dicts = response_dict['items']
+print('Repositories returned:', len(repo_dicts))
+
+#Examine the first repo.
+repo_dict = repo_dicts[0]
+print('\nKeys:', len(repo_dict))
+for key in sorted(repo_dict.keys()):
+    print(key)
